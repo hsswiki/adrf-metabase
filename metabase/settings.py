c965fa8fd9c3148f79b9836bdecb2ef4974cf1b4
Molly Rossow <mrossow@chapinhall.org>
2019-02-28 16:39:50 -0600

    Include settings file

---
+++ metabase/settings.py
@@ -0,0 +1,7 @@
+'''Settings for metabase.'''
+
+# Database connection strings for database containing data.
+metabase_connection_string = 'postgresql://metaadmin@localhost:5432/postgres'
+
+# Database connection strings for database containing data.
+data_connection_string = 'postgresql://metaadmin@localhost:5432/postgres'
